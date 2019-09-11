c79760edce6e2b69beec13e701ce433049c82734
Nathan Feitoza <nathanfeitoza@outlook.com.br>
2019-09-11 11:37:40 -0300

    Adjust to firebase5.6

--- src/Clock.php
+++ src/Clock.php
@@ -1,12 +1,10 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Kreait;
 
 use DateTimeImmutable;
 
 interface Clock
 {
-    public function now(): DateTimeImmutable;
+    public function now();
 }
